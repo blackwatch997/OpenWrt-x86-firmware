aa64181f21b6dca2b64b174c170ebe03e523e305
blackwatch997 <65154897+blackwatch997@users.noreply.github.com>
2021-12-16 16:30:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,16 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# cp -R ../default-settings package/default-settings
+
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+#rm -rf package/openwrt-passwall/xray-core
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+cd package/luci-app-openclash/tools/po2lmo
+make && sudo make install
